20562dfc2a84dd0305fd7ac513421b83a1370b20
Arjun Comar <mandaya@rose-hulman.edu>
2010-07-02 01:21:45 -0500

    Added functions to utils.clj that allow for the creation and parsing of closures.

--- src/running/core.clj
+++ src/running/core.clj
@@ -5,20 +5,10 @@
 
 (comment
   (1) Bind a thread to a port with a listener
-  (2) Wrap a closure in a map
+  (2) Create the closure
   (3) Pass it through the network to the port
   (4) The remote listener should execute the closure)
 
-(defstruct closure :closure)
-(defn make-closure-listener []
-  #())
-
-(defn bind-listener-to-port [port]
-  ())
-
-
-
-(create-server )
 
 
 
--- src/running/utils.clj
+++ src/running/utils.clj
@@ -3,5 +3,39 @@
 
 (defstruct closure :closure)
 
-(defn eval-str [s]
+(defn- eval-str [s]
   (main/main "-e" s))
+
+(defn- get-eval [map]
+  (eval (:closure map)))
+
+(defn- extract
+  ([str]
+     (with-in-str str (read))))
+
+(defn extract-eval
+  "Returns the evaluation of the closure
+  contained in the input string."
+  [str] (get-eval (extract str)))
+
+(defn store
+  "Packages the closure such that it can
+  be written to a socket, etc."
+  [c] (struct closure c))
+
+(defn store-all
+  "Packages all the closures using store.
+   Returns a list of the stored values
+   suitable for writing and reading.
+   The preferred usage."
+  ([& cs]
+     (lazy-seq (reduce conj '() (map store cs)))))
+
+(defn extract-eval-all
+  "The preferred usage. Parses the string
+   into a collection of closures, and
+   proceeds to evaluate all of them,
+   returning a list of the results."
+  ([str]
+     (let [coll (extract str)]
+       (lazy-seq (map #(get-eval %) coll)))))
